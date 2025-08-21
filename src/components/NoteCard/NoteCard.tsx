f89204abda7ebbcd5a85a7fade7d593f08b12797
kmk0304 <kmk0304@aladin.co.kr>
2025-08-21 11:11:20 +0900

    noteCard 컴포너트 생성

--- src/components/NoteCard/NoteCard.tsx
+++ src/components/NoteCard/NoteCard.tsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import type { Note } from '../../types/note'
+import { Card, ContentBox, FooterBox, TagsBox, TopBox } from './NoteCard.styles'
+import { NotesIconBox } from '../../styles/styles'
+import { BsFillPinFill } from 'react-icons/bs'
 
-const NoteCard = () => {
+interface NoteCardProps {
+  note: Note
+  type: string
+}
+
+const NoteCard = ({ note, type }: NoteCardProps) => {
+  const { title, content, tags, color, priority, isPinned, isRed, date, createTime, editedTimeid } = note
   return (
-    <div>NoteCard</div>
+    <Card
+      style={{ background: color }}>
+      <TopBox>
+        <div className='noteCard__title'>
+          {title.length > 10 ? title.slice(0, 10) + '...' : title}
+        </div>
+        <div className='nostCard__top-options'>
+          <span className='noteCard__priority'>
+            {type !== "archive" && type !== "trash" && (
+              <NotesIconBox className='noteCard__pin'>
+                <BsFillPinFill style={{color: isPinned ? "red" : ""}}/>
+              </NotesIconBox>
+            )}
+          </span>
+        </div>
+      </TopBox>
+      <ContentBox>
+        {content}
+      </ContentBox>
+      <TagsBox>
+        {tags.map(({ tag, id }) => (
+          <span key={id}>{tag}</span>
+        ))}
+      </TagsBox>
+      <FooterBox>
+        <div className='noteCard__date'>{date}</div>
+      </FooterBox>
+
+    </Card>
   )
 }
 
